98d6e7f6d3dace53f4ccf63f9e8addc84d51fed4
msli <msli@easipass.com>
2021-01-04 09:35:50 +0800

    init project

---
+++ src/main/java/com/xiaosong/myframework/MyFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.xiaosong.myframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFrameworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xiaosong/myframework/MyFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaosong.myframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFrameworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
